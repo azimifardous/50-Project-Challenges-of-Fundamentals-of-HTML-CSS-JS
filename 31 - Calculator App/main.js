5b4eed9ff54110c3da1018e82378d98a8f296a85
Ahmad Fardous Azimi <101262122+azimifardous@users.noreply.github.com>
2022-04-24 09:29:18 +0300

    Add files via upload

---
+++ 31 - Calculator App/main.js
@@ -0,0 +1,66 @@
+// Let's Make Themes - for that we have to grab the needed elements
+const ball = document.querySelector(".theme_ball");
+const themeStage = document.querySelectorAll(".theme-item");
+const totalStage = themeStage.length;
+let currentStage = 0;
+
+// Let's add a click eventListener on ball/toggler
+ball.addEventListener("click", function () {
+    // Check the current stage and increment it by one and store to the currentStage
+    if (currentStage === totalStage - 1) {
+        currentStage = 0
+    } else {
+        currentStage++;
+    }
+
+    /* Each time we click the ball the currentStage is going change to
+        theme numbers and we have to write a conditional for each stage
+    */
+    if (currentStage === 1) {
+        ball.style.transform = "translateX(20px)"
+        document.body.classList.add("theme2")
+        document.body.classList.remove("theme1")
+        document.body.classList.remove("theme3")
+    } else if (currentStage === 2) {
+        ball.style.transform = "translateX(40px)"
+        document.body.classList.add("theme3")
+        document.body.classList.remove("theme2")
+        document.body.classList.remove("theme1")
+    } else {
+        ball.style.transform = "translateX(0)"
+        document.body.classList.add("theme1")
+        document.body.classList.remove("theme2")
+        document.body.classList.remove("theme3")
+
+    }
+})
+
+// Let's ,make our calculator functional....! 🤩
+// Let's grab all the numbers and buttons
+
+const inputNumber = document.querySelector(".input");
+const numbers = document.querySelectorAll(".number");
+const deleteBtn = document.querySelector(".del");
+const resetBtn = document.querySelector(".reset-btn");
+const equalBtn = document.querySelector(".equal-btn");
+
+
+numbers.forEach((item) => {
+    item.addEventListener("click", function (e) {
+        inputNumber.textContent += e.target.value
+    })
+
+})
+
+equalBtn.addEventListener("click", function () {
+    inputNumber.textContent = eval(inputNumber.textContent)
+})
+
+resetBtn.addEventListener("click", function () {
+    inputNumber.textContent = ""
+})
+
+deleteBtn.addEventListener("click", function () {
+    inputNumber.textContent = inputNumber.textContent.slice(0, -1)
+})
+
